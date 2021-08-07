052991023652b2c8c87671b8804704621aa0a788
poornima-alugubelly <70846516+poornima-alugubelly@users.noreply.github.com>
2021-08-07 13:59:17 +0530

    feat : added highest score feature

--- Index.js
+++ Index.js
@@ -6,40 +6,44 @@ var score = 0;
 console.log(chalk.cyanBright.italic("Hi This is Poornima"));
 var userName = readlineSync.question(chalk.cyanBright.italic("What's your name ?  "));
 
-console.log(chalk.cyanBright.italic("Welcome " + chalk.magentaBright.bold(userName) + " let's see how well you know me"));
+console.log(chalk.cyanBright.italic("Welcome " + chalk.magentaBright.bold(userName) + " let's see how well you know about anime"));
 
 var scoreBoard=[{
-  name: "Tanay: ",
-  score: 0,
+  name: "Apoorva: ",
+  score: 1,
 },
 {
   name: "Sanjana:",
-  score: 4,
+  score: 3,
 }]
 
 function play(question, answer){
   var userAnswer = readlineSync.question(question);
   if(userAnswer.toUpperCase() === answer.toUpperCase()){
-    console.log("Correct !");
+    console.log(chalk.green("Correct!"));
     score = score + 1;
   }else
   {
-    console.log("Wrong Answer");
+    console.log(chalk.redBright("Wrong Answer!"));
   }
+
+ console.log(chalk.cyanBright.italic("Your current score is " + chalk.magentaBright.bold(score) )); 
 
 }
 
+console.log("-----------------------------");
+
 var questions = [{
-    question: chalk.yellowBright("Which country is anime from? \n A) China \n B) Japan \n C) USA \n D) India \n "),
+    question: chalk.yellowBright("Which country is anime from?") + chalk.blueBright("\n A) China \n B) Japan \n C) USA \n D) India \n "),
     answer: "B"
 },{
-  question: chalk.redBright("What was the first anime movie to win an oscar\n A) Your Name \n B) Wolf Children \n C)  End of Evangelion \n D) Spirited Away \n "),
+  question: chalk.yellowBright("What was the first anime movie to win an oscar?") + chalk.blueBright("\n A) Your Name \n B) Wolf Children \n C) End of Evangelion \n D) Spirited Away \n "),
   answer: "D"
 },{
- question: chalk.green("Is Mosquitoe stronger than Saitama? State Yes or No \n"),
+ question: chalk.yellowBright("Is Mosquitoe stronger than Saitama? State Yes or No \n"),
  answer: "Yes"
 },{
-  question: chalk.blueBright("What is the highest rated anime on MAL? \n A) Gintama \n B) Erased \n C) FMAB \n D) FLCL \n "),
+  question: chalk.yellowBright("What is the highest rated anime on MAL?") + chalk.blueBright("\n A) Gintama \n B) Erased \n C) FMAB \n D) FLCL \n "),
   answer: "C"
   }] ;
 
@@ -49,18 +53,23 @@ for(var i=0; i<questions.length; i++){
   currentQuestion.answer);
 }
 
-max=0
-console.log("--------");
-console.log(chalk.bgBlue.yellowBright.italic.bold("You scored: ", score));
+console.log("-------------");
+console.log("You scored: ", score);
 
-  for(var i=0;i<scoreBoard.length; i++){
-   
-         max =Math.max(scoreBoard[i].score, score);
+var max=0;
+
+for(var i=0;i<scoreBoard.length-1; i++){
+     max = Math.max(scoreBoard[i].score,scoreBoard[i+1].score)
+  } 
+console.log("Currently the highest score on this quiz is " + max);
+
+if(score>=max){
+    console.log("Congratulations! You have achieved max score 🔥")
+    console.log("Please send me a screen shot!")
   }
-    if(max<4){
-    console.log(chalk.bgWhiteBright.redBright.italic.bold("Currently the highest score on this quiz is " + max));
-    console.log(chalk.yellowBright.italic.bold("Please let me know if you score the highest "));
-    }
-    else{
-     console.log(chalk.bgWhiteBright.redBright.italic.bold(" High score!! You're an anime whiz!"));
-    }
+
+  else{
+    console.log("Missed it by " + (max-score) + " points 😔")
+  }
+
+
